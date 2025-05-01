34064feec9b0601cd4dc12135a35ec697757f48f
Ryan Saf <133062321+ishoep@users.noreply.github.com>
2025-05-01 16:24:39 +0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -71,12 +71,13 @@ const Index = () => {
             <iframe 
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1521.8676765352865!2d64.42675917320396!3d39.764777799999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f50076141cf25f9%3A0xc6120f90e030ea68!2siHelp%20Service!5e0!3m2!1sen!2sus!4v1683894531117!5m2!1sen!2sus" 
               width="100%" 
-              height="100%" 
-              style="border:0;" 
-              allowfullscreen="" 
+              height="250" 
+              style={{ border: 0 }} 
+              allowFullScreen 
               loading="lazy" 
-              referrerpolicy="no-referrer-when-downgrade"
+              referrerPolicy="no-referrer-when-downgrade"
               title="iHelp Service Location"
+              className="w-full"
             ></iframe>
           </div>
         </div>
